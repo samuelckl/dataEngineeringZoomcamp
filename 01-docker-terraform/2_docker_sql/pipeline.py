9879cd8459161842d190a434da904d87e89f492d
samuelckl <59119742+samuelckl@users.noreply.github.com>
2025-01-28 15:31:42 +1100

    Added the files and also the SQL queries answer
    Couldnt upload the data folder and csv too big to upload

---
+++ 01-docker-terraform/2_docker_sql/pipeline.py
@@ -0,0 +1,20 @@
+# import sys
+
+# import pandas as pd
+
+# print(sys.argv)
+
+# day = sys.argv[1]
+
+# # some fancy stuff with pandas
+
+# print(f'job finished successfully for day = {day}')
+
+# self exercise
+
+import sys
+import pandas as pd
+print(sys.argv)
+day = sys.argv[1]
+
+print(f'job finished successfully for day = {day}')
